eeb770068e2b0717dfed3614fcdeaafbdf4b2672
Smartguy88 - Smg88 <96052597+Smartestguy88@users.noreply.github.com>
2022-05-22 16:21:12 +1000

    Setting up ... 6 (Arduino Uploads now work :)

---
+++ IOT Integration Code/C Plus Plus/ESP32 Specific Libraries/AHI/AHI.ino
@@ -0,0 +1,26 @@
+#define VERSION_WARNINGS
+
+#ifdef VERSION_WARNINGS
+#warning AHI.ino version: 0.0.1 // SCAN VERSION
+#endif
+
+//#define IMPLEMENTATION
+#define INFO
+#define DEBUG
+//#define DONT_USE_SERIAL
+
+#include <Arduino.h>
+
+#define IRCODES_INCLUDE_CANDLE
+
+#include <IRcodes.h>
+
+void setup()
+{
+  ;
+}
+
+void loop()
+{
+  ;
+}
