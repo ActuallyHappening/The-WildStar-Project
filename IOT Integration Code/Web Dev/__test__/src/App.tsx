ea3e590413a20c8f32427ddf479d40f76e000059
Smartguy88 - Smg88 <96052597+Smartestguy88@users.noreply.github.com>
2022-05-29 13:11:15 +1000

    Well restarting __test__ experiment to get better liftoff!

--- IOT Integration Code/Web Dev/__test__/src/App.tsx
+++ IOT Integration Code/Web Dev/__test__/src/App.tsx
@@ -1,23 +1,10 @@
 import { useState } from 'react'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Basic IOT Integration Code</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-      </header>
-    </div>
+    <div className='TopLevel bg-green-50 text-center'><p>Yay!</p></div>
   )
 }
 
