09f463d69e49205ec6d41d1d3305f3a8a5f09d3b
Smartguy88 - Smg88 <96052597+Smartestguy88@users.noreply.github.com>
2022-06-06 16:46:31 +1000

    Refactoring Commands.py to include timeout feature ...

--- IOT Integration Code/MicroPython/Integration Code/Code/Commands/Commands.py
+++ IOT Integration Code/MicroPython/Integration Code/Code/Commands/Commands.py
@@ -1,5 +1,7 @@
 
 import time
+
+from sqlalchemy import over
 import uasyncio as asio
 
 from . import MachineCommands
@@ -10,9 +12,20 @@ from .command import Command as CommandClass
 prebuilt = dict()
 
 
+async def _timeoutWrapper(coroutine):
+    async def __timeoutWrapper(*_args, time=..., **_kwargs):
+        if time is not ...:
+            await asio.wait_for(coroutine(*_args, *_kwargs), timeout=time)
+        else:
+            print(
+                f"#>\tRunning __timeoutWrapper with no given time, defaulting to asio.gather(...)")
+            await asio.gather(coroutine(*_args, *_kwargs))
+    return __timeoutWrapper
+
+
 def importSet(commands):
     for name, command in commands.items():
-        prebuilt[name] = command
+        prebuilt[name] = _timeoutWrapper(command)
         if command.name is None:
             command.name = name  # Set name of command to handle used to call it :)
 
@@ -22,7 +35,7 @@ importSet(TimeCommands.commands)
 importSet(ServerCommands.commands)
 
 
-async def _execute(commands, **overflow):
+async def _execute(commands, time=..., **overflow):
     if len(overflow) > 0:
         print(f"OH oh, overflow detected in func _execute: {overflow=}")
     if type(commands) is not list:
@@ -36,8 +49,10 @@ async def _execute(commands, **overflow):
             continue
         print(
             f"#\tMaking Task {str(command.name)+' ' if hasattr(command,'name') else '__'}...")
+    if time is ...:
+        print(f"#> Note: {time=}, is not set, running until completion")
     print(f"## Holding tasks ({time} seconds) ...")
-    await asio.gather(*[task.asioDo() for task in commands])
+    await asio.gather(*[task.asioDo(time=time) for task in commands])
     print("## Released tasks.")
 
 
